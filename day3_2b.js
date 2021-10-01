44712a167fe254efa541dddb9295fbe289d6f77c
Upendra12395 <ch95upendra@gmail.com>
2021-10-01 19:53:12 +0530

    problem 5

---
+++ day3_2b.js
@@ -0,0 +1,94 @@
+const users = [
+    {
+            _id: 'ab12ex',
+            username: 'Alex',
+            email: 'alex@alex.com',
+            password: '123123',
+            createdAt:'17/05/2019 9:00 AM',
+            isLoggedIn: false
+    },
+    {
+            _id: 'fg12cy',
+            username: 'Asab',
+            email: 'asab@asab.com',
+            password: '123456',
+            createdAt:'17/05/2019 9:30 AM',
+            isLoggedIn: true
+    },
+    {
+            _id: 'zwf8md',
+            username: 'Brook',
+            email: 'brook@brook.com',
+            password: '123111',
+            createdAt:'17/05/2019 9:45 AM',
+            isLoggedIn: true
+    },
+    {
+            _id: 'eefamr',
+            username: 'Martha',
+            email: 'martha@martha.com',
+            password: '123222',
+            createdAt:'17/05/2019 9:50 AM',
+            isLoggedIn: false
+    },
+    {
+            _id: 'ghderc',
+            username: 'Thomas',
+            email: 'thomas@thomas.com',
+            password: '123333',
+            createdAt:'17/05/2019 10:00 AM',
+            isLoggedIn: false
+    }
+    ];
+
+    const products = [
+{
+    _id: 'eedfcf',
+    name: 'mobile phone',
+    description: 'Huawei Honor',
+    price: 200,
+    ratings: [
+        { userId: 'fg12cy', rate: 5 },
+        { userId: 'zwf8md', rate: 4.5 }
+    ],
+    likes: []
+},
+{
+    _id: 'aegfal',
+    name: 'Laptop',
+    description: 'MacPro: System Darwin',
+    price: 2500,
+    ratings: [],
+    likes: ['fg12cy']
+},
+{
+    _id: 'hedfcg',
+    name: 'TV',
+    description: 'Smart TV:Procaster',
+    price: 400,
+    ratings: [{ userId: 'fg12cy', rate: 5 }],
+    likes: ['fg12cy']
+}
+];
+
+//a. Create a function called ***rateProduct*** which rates the product
+function rateProduct(product, rating){
+ let existProduct = products.find(findproduct => findproduct.name == product)
+ existProduct.ratings.push(rating)
+ console.log(existProduct)
+}
+
+rateProduct('TV', {userId : 'ghderc', rate: 4})
+
+//b. Create a function called ***averageRating*** which calculate the average rating of a product
+function averageRating(product){
+    let existProduct = products.find(findproduct=> findproduct.name == product)
+    let sumOfRating = 0,i;
+    for ( i =0; i<existProduct.ratings.length; i++){
+        sumOfRating+=existProduct.ratings[i].rate;
+    }
+    let avgRating = sumOfRating/i;
+    console.log(avgRating)
+}
+
+averageRating('TV')
